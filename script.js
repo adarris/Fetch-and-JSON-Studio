849a0d15250fd20b30b52d6a971b7bf81c6a18db
Adriana Darris <adrianadarris@Adrianas-Air.lan>
2022-07-31 23:58:14 -0500

    completed studio

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // TODO: add code here
+
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then((response) => {
+        return response.json();
+}).then((json) => {
+            let theContainer = document.getElementById("container");
+            let astronauts = '';
+            for (astronaut of json) {
+                astronauts += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                                <li>Active: ${astronaut.active}</li>
+                                <li>Skills: ${astronaut.skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${astronaut.picture}">
+                    </div>
+                `;
+            }
+            theContainer.innerHTML = astronauts;
+        });
+    });
